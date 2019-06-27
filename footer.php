7d540c79a7d143b7d7b4dbb41e145e0259662fe0
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-06-27 23:52:32 +0530

    Adding footer file to project

---
+++ footer.php
@@ -0,0 +1,8 @@
+        <div class="container-fluid footer" style="padding: 15px; font-size: 20px; color: red; background-color: #ffa827;">
+            <center>&copy; Copyright Reserved</center>
+        </div>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> 
+<!--   <script src="Personal-Validation.js"></script>-->
+    </body>
+</html>
