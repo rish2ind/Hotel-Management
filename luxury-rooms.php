7e625d2f9496732e910ff52812e2fe749b350936
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-06-27 23:45:00 +0530

    Rooms details

---
+++ luxury-rooms.php
@@ -0,0 +1,37 @@
+<?php
+    include('header.php');
+?>
+
+<body style="background-image: url('Images/supirior1.jpg'); background-size: cover;">
+    
+
+<div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Luxury Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/luxury.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green; text-shadow: 2px 3px 35px;">Facilities</h2><br>
+                    
+<ul style="font-size: 18px;"><li>
+    Free Wifi
+</li>
+<li>TV with 36 Indian and International Channels</li>
+<li>Minibar</li>
+<li>Refrigerator</li>
+<li>Air Condition</li>
+<li>Luxury branded Carpe Diem bed</li>
+<li>Bathtub and Shower</li>
+    <li>Cosmetic Mirror in bathroom</li></ul>
+<span style="color: red; font-size:18px;"> Price : &nbsp;6000 /-</span>
+               <a href="rooms-booking.php"><button class="btn btn-success" style="padding: 15px; float: right; border-radius: 10px;">Book</button></a>
+                </div>
+            </div>
+        </div>
+</body>        
+       <?php
+    include('footer.php');
+?>
---
+++ superior-rooms.php
@@ -0,0 +1,32 @@
+<?php
+    include('header.php');
+?>
+        <body style="background-image: url('Images/supirior1.jpg'); background-size: cover;"> 
+        <div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Standard Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/Superior.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green; text-shadow: 2px 3px 35px;">Facilities</h2><br>
+                   <ul style="font-size: 18px;">
+                       <li>
+                           Breakfast
+                       </li>
+                       <li>Air Condition</li>
+                       <li>Free Wifi</li>
+                       <li>Laundry Facility</li>
+                   </ul>
+<span style="color: red; font-size: 18px;">SINGLE 2000 /- &nbsp;&nbsp;DOUBLE 2500 /-</span>
+               <a href="rooms-booking.php"><button class="btn btn-success" style="padding: 15px; float: right; border-radius: 10px;">Book</button></a>
+                </div>
+            </div>
+        </div>
+            
+</body>      
+       <?php
+        include('footer.php');
+?>
