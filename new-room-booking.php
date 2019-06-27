a3f5ae0f09a2bebc1fee2761e6e3a86a7ddc6704
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-06-27 23:42:14 +0530

    Booking section of rooms

---
+++ new-room-booking.php
@@ -0,0 +1,35 @@
+<?php 
+    include('header.php');
+?>
+        
+        <div class="container-fluid"><img src="Images/background1.jpg" alt="" class="img-responsive" height="200px;"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="details"><p class="rooms">Standard</p><a href="superior-rooms.php"><img src="Images/Superior.jpg" alt="" class="img-responsive" style="height: 196.31px;"></a><p class="rooms-inner">These executive rooms are well suited for business travelers. The rooms are voguish and tastefully designed with the sole aim of making it a perfect business suite..</p></div>
+        </div>
+        <div class="col-md-3">
+            <div class="details"><p class="rooms">Delux</p><a href="delux-rooms.php"><img src="Images/delux.jpg" alt="" class="img-responsive" style="height: 196.31px;"></a><p class="rooms-inner">The deluxe rooms come equipped with all the luxury facilities with a very personal touch which makes them a favourite amongst our corporate guests.</p></div>
+        </div>
+        <div class="col-md-3">
+            <div class="details"><p class="rooms">Single</p><a href="single-rooms.php"><img src="Images/Single.jpg" alt="" class="img-responsive" style="height: 196.31px;"></a><p class="rooms-inner">A single room is a room intended for one person to stay in. Choose from twin or single rooms, all of which are comfortable. Each guest has her own single room, or shares a double room.</p></div>
+        </div>
+        <div class="col-md-3">
+            <div class="details"><p class="rooms">Luxury</p><a href="luxury-rooms.php"><img src="Images/luxury.jpg" alt="" class="img-responsive" style="height: 196.31px;"></a><p class="rooms-inner">A perfect blend of luxury and comfort, Luxury Room provides a soothing and warm turquoise ambiance to each guests with carefully chosen.</p></div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?php
+    include('footer.php');
+?>
